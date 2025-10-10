6d0ee49cb1ffb01bf71aafe5931fb080bc52372d
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-10-10 13:00:49 +0200

    Update merge

--- Src/SonarQubeWebApiShare/SonarQube.cs
+++ Src/SonarQubeWebApiShare/SonarQube.cs
@@ -1,47 +1,42 @@
 ﻿namespace SonarQubeWebApi;
 
-public class SonarQube : JsonBaseClient, IDisposable
+public sealed class SonarQube : JsonService
 {
-    private SonarQubeService? service;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="SonarQube"/> class using a store key and application name.
-    /// </summary>
-    /// <param name="storeKey">The key to retrieve the host and token from the key store.</param>
-    /// <param name="appName">The name of the application using the API.</param>
-    public SonarQube(string storeKey, string appName)
-    : this(new Uri(KeyStore.Key(storeKey)?.Host!), KeyStore.Key(storeKey)!.Token!, appName)
+    ///// <summary>
+    ///// Initializes a new instance of the <see cref="SonarQube"/> class using a store key and application name.
+    ///// </summary>
+    ///// <param name="storeKey">The key to retrieve the host and token from the key store.</param>
+    ///// <param name="appName">The name of the application using the API.</param>
+    public SonarQube(string storeKey, string appName) : base(storeKey, appName, SourceGenerationContext.Default)
+    { }
+       
+    public SonarQube(Uri host, IAuthenticator? authenticator, string appName) : base(host, authenticator, appName, SourceGenerationContext.Default)
     { }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="SnipeIT"/> class using a host URI, token, and application name.
-    /// </summary>
-    /// <param name="host">The base URI of the Snipe-IT API.</param>
-    /// <param name="token">The authentication token for the API.</param>
-    /// <param name="appName">The name of the application using the API.</param>
-    public SonarQube(Uri host, string token, string appName)
-    {
-        service = new(host, new BearerAuthenticator(token), appName);
-    }
+    protected override string? AuthenticationTestUrl => "api/server/version";
 
-    /// <summary>
-    /// Disposes the resources used by the <see cref="SnipeIT"/> instance.
-    /// </summary>
-    public void Dispose()
-    {
-        if (this.service != null)
-        {
-            this.service.Dispose();
-            this.service = null;
-        }
-        GC.SuppressFinalize(this);
-    }
-
-    //public async Task<Version?> GetVersionAsync(CancellationToken cancellationToken = default)
+    //protected override async Task ErrorCheckAsync(HttpResponseMessage response, string memberName, CancellationToken cancellationToken)
     //{
-    //    WebServiceException.ThrowIfNullOrNotConnected(this.service);
+    //    await base.ErrorCheckAsync(response, memberName, cancellationToken);
 
-    //    var res = await service.GetVersionAsync(cancellationToken);
-    //    return new Version(res!);
+    //    string str = await response.Content.ReadAsStringAsync(cancellationToken);
+    //    if (str.StartsWith("{\"status\":\"error\""))
+    //    {
+    //        //var res = await ReadFromJsonAsync<ResultModel>(response, cancellationToken);
+
+    //        JsonTypeInfo<ResultModel> jsonTypeInfo = (JsonTypeInfo<ResultModel>)context.GetTypeInfo(typeof(ResultModel))!;
+    //        var res = await response.Content.ReadFromJsonAsync<ResultModel>(jsonTypeInfo, cancellationToken);
+
+    //        throw new WebServiceException(res!.Messages);
+    //    }
     //}
+
+    public override async Task<string?> GetVersionStringAsync(CancellationToken cancellationToken = default)
+    {
+        WebServiceException.ThrowIfNotConnected(client);
+
+        var res = await GetStringAsync("api/server/version", cancellationToken);
+        return res;
+    }
 }
+
