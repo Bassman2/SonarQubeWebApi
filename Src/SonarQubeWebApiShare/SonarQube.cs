ed1583885879c1fc9f8e8ece0c86448075f39657
Ralf Beckers <ralf.beckers@elektrobit.com>
2025-12-16 12:10:53 +0100

    Update

--- Src/SonarQubeWebApiShare/SonarQube.cs
+++ Src/SonarQubeWebApiShare/SonarQube.cs
@@ -13,6 +13,18 @@ public sealed class SonarQube : JsonService
     public SonarQube(Uri host, IAuthenticator? authenticator, string appName) : base(host, authenticator, appName, SourceGenerationContext.Default)
     { }
 
+    /// <summary>
+    /// Configures the provided <see cref="HttpClient"/> instance with specific default headers required for API requests.
+    /// This includes setting the User-Agent, Accept, and API version headers.
+    /// </summary>
+    /// <param name="client">The <see cref="HttpClient"/> to configure for GitHub API usage.</param>
+    /// <param name="appName">The name of the application, used as the User-Agent header value.</param>
+    protected override void InitializeClient(HttpClient client, string appName)
+    {
+        client.DefaultRequestHeaders.Add("User-Agent", appName);
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+    }
+
     protected override string? AuthenticationTestUrl => "api/server/version";
 
     //protected override async Task ErrorCheckAsync(HttpResponseMessage response, string memberName, CancellationToken cancellationToken)
